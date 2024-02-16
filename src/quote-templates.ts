87d8f154c6075f560d62966021d62e7e85bddea2
Andriy Pashynnyk <tobirawork@gmail.com>
2024-02-16 22:16:55 +0200

    quotes

--- src/quote-templates.ts
+++ src/quote-templates.ts
@@ -12,4 +12,11 @@ export const QUOTE_TEMPLATES = {
   whenHeSays({ mid, banger, him }: QuoteTmplateArgs) {
     return `When he says "${mid}", but ${him} said "${banger}"`;
   },
+
+  howBeautifulWasIt({ mid, banger, him }: QuoteTmplateArgs) {
+    return `How beautiful was it when ${him} said this: "${banger}"`;
+  },
+
+  // i realy felt it when {him} said "{banger}"
+  // {him} is whild for saying "{banger}"
 };
---
+++ src/quotes.ts
@@ -0,0 +1,15 @@
+type Quote = {
+  text: string;
+  author: string;
+};
+
+export const QUOTES: Quote[] = [
+  {
+    author: "Howie Day",
+    text: "Meeting you was a coincidence, becoming friends was a choice, but falling in love with you was beyond my control.",
+  },
+  {
+    author: "Rainbow Rowell",
+    text: "She never looked nice. She looked like art, and art wasn't supposed to look nice; it was supposed to make you feel something.",
+  },
+];
